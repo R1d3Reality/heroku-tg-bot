cb200120402b8fca3b80d7e58091c633a4d43d17
Pasha Stefiuk <r1d3rreality@gmail.com>
2018-10-23 12:32:18 +0200

    v2.0.2

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const TOKEN = '763838619:AAE0h1CsibGj51v53YtCqx6dLSiCg4DXvlM'
 const bot = new TelegramBot(TOKEN, {polling: true})
 
 bot.on('message', msg => {
-	bot.sendMessage(msg.chat.id, `Hello from HEROKU, bot says: "Hi, ${msg.from.firstname}"`)
+	bot.sendMessage(msg.chat.id, `Hello from HEROKU, bot says: "Hi, ${msg.from.chat_id}"`)
 })
