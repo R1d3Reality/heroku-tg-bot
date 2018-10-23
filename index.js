feae2cdfe3d15fa2db7bc77edfc479e84be252a5
Pasha Stefiuk <r1d3rreality@gmail.com>
2018-10-23 13:58:06 +0200

    study v5

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ const TOKEN = '763838619:AAE0h1CsibGj51v53YtCqx6dLSiCg4DXvlM'
 const bot = new TelegramBot(TOKEN, {polling: true})
 
 bot.on('message', msg => {
-  bot.sendMessage(msg.chat.id, `${msg.from.username} Вас вітає фотограф БлаБлаБла`)
+ bot.SendMessage(chat_id, "1")
   .then(() => {
-  return bot.SendMessage(msg.chat.id, `${msg.from.username} хочете зареєструватись на фотосесію?`)
-    )}
-})
+    return bot.SendMessage(chat_id, "2")
+  })
+  .then(() => {
+    return bot.SendMessage(chat_id, "3")
+  });
+});
+
 
